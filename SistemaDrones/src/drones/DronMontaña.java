06abd85c6d0d2f75f12c2ee928e351277ddef063
Adrian Cuesta <AdrianCuesta@ELP69>
2026-03-06 23:21:38 +0100

    Dron montaña creado

---
+++ SistemaDrones/src/drones/DronMontaña.java
@@ -0,0 +1,36 @@
+package drones;
+
+public class DronMontaña extends DronBase {
+
+	private int altitud;
+
+	public DronMontaña(String identificador, int autonomiaMaxima, int velocidadMaxima, boolean estaOperativo,
+			int altitud) {
+		super(identificador, autonomiaMaxima, velocidadMaxima, estaOperativo);
+
+		if (altitud < 0) {
+			throw new IllegalArgumentException("La altitud no puede ser negativa");
+		}
+
+		this.altitud = altitud;
+
+	}
+
+	@Override
+	public double calcularTiempoRespuesta(int distanciaMision) {
+
+		if (distanciaMision < 0) {
+			throw new IllegalArgumentException("La distancia de la mision no puede ser negativa");
+		}
+		super.comprobarOperatividad();
+		int tiempoRespuesta = (distanciaMision / super.getVelocidadMaxima() * altitud);
+
+		if (tiempoRespuesta > super.getAutonomiaMaxima()) {
+			throw new IllegalArgumentException("El tiempo de respuesta supera la autonomia del dron");
+		}
+
+		return tiempoRespuesta;
+
+	}
+
+}
