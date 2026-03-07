e3e06df3153be644954d585f22b86581afafcad9
Adrian Cuesta <AdrianCuesta@ELP69>
2026-03-07 10:03:42 +0100

    Documentacion dron montaña

--- SistemaDrones/src/drones/DronMontaña.java
+++ SistemaDrones/src/drones/DronMontaña.java
@@ -1,8 +1,28 @@
 package drones;
 
+/**
+ * Clase DronMontaña que representa a un dron de tipo montaña. La clase esta
+ * especializada a partir de {@link DronBase} Incluye informacion específica
+ * sobre la altitud
+ */
 public class DronMontaña extends DronBase {
-
+	/**
+	 * El factor de altitud del dron
+	 */
 	private double altitud;
+
+	/**
+	 * Constructor con 5 parametros
+	 * 
+	 * @param identificador   Identificador del dron
+	 * @param autonomiaMaxima La autonomía máxima del dron
+	 * @param velocidadMaxima La velocidad máxima alcanzada por el dron
+	 * @param estaOperativo   Indica si el dron se encuentra funcional o no
+	 * @param altitud         Es el factor de altitud
+	 * 
+	 * @throws IllegalArgumentException Si el factor de altitud introducido es
+	 *                                  negativo
+	 */
 
 	public DronMontaña(String identificador, int autonomiaMaxima, int velocidadMaxima, boolean estaOperativo,
 			double altitud) {
@@ -15,6 +35,22 @@ public class DronMontaña extends DronBase {
 		this.altitud = altitud;
 
 	}
+
+	/**
+	 * Método que calcula el tiempo de respuesta
+	 * 
+	 * @param distanciaMision La distancia que tiene que recorrer el dron
+	 * @return El tiempo que tardaría en recorrer dicha distancia
+	 * 
+	 * @throws IllegalArgumentException si:
+	 *                                  <ul>
+	 *                                  <li>La distancia de la mision es
+	 *                                  negativa</li>
+	 *                                  <li>El tiempo de respuesta supera la
+	 *                                  autonomia del dron</li>
+	 *                                  </ul>
+	 * @throws IllegalStateException    Si el dron no se encuentra operativo
+	 */
 
 	@Override
 	public double calcularTiempoRespuesta(int distanciaMision) {
@@ -33,10 +69,20 @@ public class DronMontaña extends DronBase {
 
 	}
 
+	/**
+	 * Devuelve el factor de altitud del dron
+	 * 
+	 * @return El factor de altitud
+	 */
 	public double getAltitud() {
 		return altitud;
 	}
 
+	/**
+	 * Inserta un nuevo factor de altitud al dron
+	 * 
+	 * @param altitud Nuevo factor de altitud
+	 */
 	public void setAltitud(int altitud) {
 		this.altitud = altitud;
 	}
